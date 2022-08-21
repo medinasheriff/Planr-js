5d3f560090482873f3f0fcf5f91a848a9836738f
Medina Sheriff <medlin142@gmail.com>
2022-08-21 22:18:37 +0100

    add js file

---
+++ src/script.js
@@ -0,0 +1,11 @@
+let failed = document.getElementsByClassName('red');
+failed[0].style.color = "red";
+failed[1].style.color = "red";
+
+let ongoing = document.getElementsByClassName('yellow');
+ongoing[0].style.color = "yellow";
+ongoing[1].style.color = "yellow";
+
+let success = document.getElementsByClassName('green');
+success[0].style.color = "green";
+success[1].style.color = "green";
